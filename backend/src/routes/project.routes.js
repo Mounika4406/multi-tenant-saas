c210e7c9f05eee198989d2c6c356d2c9d5ec2673
Mounika4406 <23MH1A4406@acoe.edu.in>
2025-12-24 14:52:21 +0530

    Phase 6: Update project API

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -3,11 +3,14 @@ import authMiddleware from "../middleware/auth.middleware.js";
 import {
   createProject,
   listProjects,
+   updateProject,
 } from "../controllers/project.controller.js";
 
 const router = express.Router();
 
 router.post("/projects", authMiddleware, createProject);
 router.get("/projects", authMiddleware, listProjects);
+router.put("/projects/:projectId", authMiddleware, updateProject);
+
 
 export default router;
